aa42bf0e339e8e053728ee89a875e3fd5057f964
manas-gandhi <manas@gkmit.co>
2024-12-12 16:42:40 +0530

    feat(transactions): add category module - Add CRUD functionality for categories in transactions app.

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,3 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet
+
+categories_router = DefaultRouter()
+categories_router.register(r'', CategoryViewSet, basename="categories")
+
 urlpatterns = [
     
+]
+transactions_urlpatterns = [
+    
+]
+categories_urlpatterns = [
+    path('', include(categories_router.urls))
 ]
